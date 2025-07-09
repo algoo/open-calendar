d295142273e413a05d8daac702d77204454b0148
Corentin <88098360+Spiky-73@users.noreply.github.com>
2025-07-09 14:48:23 +0200

    Fix: feedback from the 2025-07-07 (#6)
    * fix: popup closing when dragging a click outside the popup
    
    * fix: labels in CalendarSelectDropdown missing ids, escape event location
    
    * fix: form-content items alignment

--- src/helpers/dom-helper.ts
+++ src/helpers/dom-helper.ts
@@ -4,3 +4,8 @@ export function parseHtml<N extends ChildNode = ChildNode>(html: string, format?
   html = Mustache.render(html, format)
   return Document.parseHTMLUnsafe(html).body.childNodes as NodeListOf<N>
 }
+
+export function escapeHtml(html: string): string {
+  // NOTE - CJ - 2025-07-07 - In Mustache, {{html}} escapes html whereas {{{html}}} and {{&html}} do not
+  return Mustache.render('{{html}}', { html })
+}
